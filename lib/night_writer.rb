84c90b155827eb7dd127899082b2ff38663f719b
cminnerath <cminnerath@gmail.com>
2015-09-10 15:06:13 -0600

    input work

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,6 +1,12 @@
 class NightWriter
-  def initialize
-    @input = ARGV[0]
+  attr_accessor :array
+
+  def initialize(input = ARGV[0])
+    @input  = input
     @output = ARGV[1]
+  end
+
+  def take_input
+    @array = File.readlines("#{@input}", "r")
   end
 end
--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -1,4 +1,10 @@
 require './lib/night_writer'
+require 'minitest/autorun'
+require 'minitest/pride'
 
 class NightWriterTest < Minitest::Test
+  def test_char_map_has_a_value_to_match_to_keyit_can_take_in_a_braille_input_file
+    testwriter = NightWriter.new("./data/input_fixture.txt")
+    assert_equal "", testwriter.take_input
+  end
 end
