c1a0d10c9b5d63af79208966614a355af7a430dc
cminnerath <cminnerath@gmail.com>
2015-09-11 04:10:00 -0600

    input_split

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,12 +1,22 @@
 class NightWriter
   attr_accessor :array
+  attr_reader :array1, :array2, :array3
 
   def initialize(input = ARGV[0])
     @input  = input
     @output = ARGV[1]
+    @array = []
+    take_input
   end
 
   def take_input
-    @array = File.readlines("#{@input}", "r")
+    File.read("#{@input}").each_line do |line|
+      @array << line.chop
+    end
+    @array.each_slice(1).with_index(1) do |group, index|
+      instance_variable_set "@array#{index}".to_sym, group
+    end
+    @array
   end
+
 end
--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -3,8 +3,15 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class NightWriterTest < Minitest::Test
-  def test_char_map_has_a_value_to_match_to_keyit_can_take_in_a_braille_input_file
+  def test_it_can_take_in_a_braille_input_file_and_seperate_lines
     testwriter = NightWriter.new("./data/input_fixture.txt")
-    assert_equal "", testwriter.take_input
+    assert_equal ["0.0.0.0.0....00.0.0.00", "00.00.0..0..00.0000..0", "....0.0.0....00.0.0.."], testwriter.array
+  end
+
+  def test_it_can_split_the_input_file_into_different_arrays
+    testwriter = NightWriter.new("./data/input_fixture.txt")
+    assert_equal ["0.0.0.0.0....00.0.0.00"], testwriter.array1
+    assert_equal ["00.00.0..0..00.0000..0"], testwriter.array2
+    assert_equal ["....0.0.0....00.0.0.."],  testwriter.array3
   end
 end
